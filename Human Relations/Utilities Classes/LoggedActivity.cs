333c03523642a838df3cfcff75f9208616a3f410
jsulumich <68516474+jsulumich@users.noreply.github.com>
2020-10-18 14:29:04 -0400

    added comments to class

--- Human Relations/Utilities Classes/LoggedActivity.cs
+++ Human Relations/Utilities Classes/LoggedActivity.cs
@@ -5,13 +5,22 @@ public class LoggedActivity
 {
    public LoggedActivity() { }
 
-    public bool logActivity(int activityType, int affectedUserID, int refID, DateTime created, int createdBy)
+
+    /*
+     DESCRIPTION: Adds row to ActivityLog table in database.
+     PARAMETERS: ActivityType: ActivityTypeID (value from dbo.activitytype table) 
+                 EffectedUserID: The UserID of the user effected by the change
+                 RefID: optional; primary key of row inserted/updated by activity
+                 Created: DateTime activity is created
+                 CreatedBY: UserID of user carrying out activity
+     */
+    public bool logActivity(int activityType, int effectedUserID, int refID, DateTime created, int createdBy)
     {
         DBConnect logActivityConn = new DBConnect();
         MySqlCommand logActivity = new MySqlCommand(@"INSERT INTO `dbo`.`activitylog`
-                                                 (`affectedUser`,`activityTypeID`,`refID`,`created`,`createdBy`)
-                                                 VALUES(@affectedUser,@activityType,@refID,@created,@createdBy)");
-        logActivity.Parameters.Add("@userID", MySqlDbType.Int32).Value = affectedUserID;
+                                                 (`effectedUser`,`activityTypeID`,`refID`,`created`,`createdBy`)
+                                                 VALUES(@effectedUser,@activityType,@refID,@created,@createdBy)");
+        logActivity.Parameters.Add("@effectedUser", MySqlDbType.Int32).Value = effectedUserID;
         logActivity.Parameters.Add("@activityType", MySqlDbType.Int32).Value = activityType;
         logActivity.Parameters.Add("@refID", MySqlDbType.Int32).Value = refID;
         logActivity.Parameters.Add("@created", MySqlDbType.Date).Value = created;
