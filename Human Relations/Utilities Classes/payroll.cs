e1868231fe5cd6f4fa59450a75bace14affe1de8
jsulumich <68516474+jsulumich@users.noreply.github.com>
2020-11-01 21:44:45 -0500

    payroll class

---
+++ Human Relations/Utilities Classes/payroll.cs
@@ -0,0 +1,32 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Human_Relations.Utilities_Classes
+{
+    class payroll
+    {
+        public int getCurrentPayPeriod(int month, int day) 
+        {
+            int payPeriodID;
+
+            // SQL Query
+            DBConnect payPeriodConn = new DBConnect();
+            MySqlCommand cmd = new MySqlCommand(@"SELECT payPeriodID 
+                                                FROM dbo.payperiods
+                                                where monthno = @month
+                                                and @day between startDate and endDate");
+            
+            // use month and day parameters in query
+            cmd.Parameters.Add("@month", MySqlDbType.Int32).Value = month;
+            cmd.Parameters.Add("@day", MySqlDbType.Int32).Value = day;
+            payPeriodID = payPeriodConn.intScalar(cmd);
+            
+            // return current pay Period
+            return payPeriodID;
+        }
+    }
+}
