90b1743bbf8554939734e86366ba08b60b6c72b8
jsulumich <68516474+jsulumich@users.noreply.github.com>
2020-11-28 16:42:53 -0500

    update admin leave page

--- Human Relations/Pages/adminLeave.cs
+++ Human Relations/Pages/adminLeave.cs
@@ -13,17 +13,23 @@ namespace Human_Relations.Pages
 {
     public partial class adminLeave : Form
     {
+        DBConnect connection = new DBConnect();
+        DataTable pendingLeave = new DataTable();
+        BindingSource bindingSource = new BindingSource();
+        MySqlCommand cmd = new MySqlCommand();
+
         public adminLeave()
         {
             InitializeComponent();
-            //display pending leave requests (oldest first) when the page opens
+            displayPendingRequests();
+
+        }
+
+// DESCRIPTION: Displays all pending leave requests (oldest to newest, by start date of request)
+        private void displayPendingRequests()
+        {
             try
             {
-                DBConnect connection = new DBConnect();
-                DataTable pendingLeave = new DataTable();
-                BindingSource bindingSource = new BindingSource();
-                MySqlCommand cmd = new MySqlCommand();
-
                 cmd.CommandText = @"select
                                     lm.userID as 'User ID',
                                     concat(emp.firstName, ' ', emp.lastName) as 'Employee',
@@ -35,7 +41,7 @@ namespace Human_Relations.Pages
                                     join dbo.user emp
                                         on emp.userID = lm.userID
                                     where lm.approvalstatus = 'Pending'
-                                    order by lm.created";
+                                    order by lm.dateStart";
                 pendingLeave = connection.ExecuteDataTable(cmd);
                 bindingSource.DataSource = pendingLeave;
                 adminLeaveDataGrid.DataSource = bindingSource;
@@ -45,18 +51,61 @@ namespace Human_Relations.Pages
             {
                 MessageBox.Show(err.ToString());
             }
-
         }
 
+        // DESCRIPTION: Displays all leave requests (oldest to newest, by start date of request)
+        private void displayAllRequests()
+        {
+            try
+            {
+                cmd.CommandText = @"select
+                                    lm.userID as 'User ID',
+                                    concat(emp.firstName, ' ', emp.lastName) as 'Employee',
+                                    lm.dateStart as 'Request Start',
+                                    lm.dateEnd as 'Request End',
+                                    DATE(lm.created) as 'Requested On',
+                                    lm.approvalStatus
+                                    from dbo.leavemgmt lm
+                                    join dbo.user emp
+                                        on emp.userID = lm.userID
+                                    order by lm.dateStart";
+                pendingLeave = connection.ExecuteDataTable(cmd);
+                bindingSource.DataSource = pendingLeave;
+                adminLeaveDataGrid.DataSource = bindingSource;
+                adminLeaveDataGrid.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.ToString());
+            }
+        }
+        // DESCRIPTION: Logs user out of application
         private void btnLogOut_Click(object sender, EventArgs e)
         {
             this.Close();
             Application.OpenForms["Menu"].Close();
         }
 
+        //DESCRIPTION: Returns user to menu screen
+
         private void btnReturn_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        // DESCRIPTION: Handler for when checkbox is checked
+        private void checkAllRequests_CheckedChanged(object sender, EventArgs e)
+        {
+            // if checked
+            if(checkAllRequests.Checked == true)
+            {
+                displayAllRequests();
+            }
+            //if not checked
+            else if(checkAllRequests.Checked == false)
+            {
+                displayPendingRequests();
+            }
         }
     }
 }
