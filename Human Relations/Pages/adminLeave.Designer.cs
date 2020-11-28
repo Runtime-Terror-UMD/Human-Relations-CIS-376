a15d48393736e840b47c5598691b7123bcc6f7ff
jsulumich <68516474+jsulumich@users.noreply.github.com>
2020-11-28 16:35:50 -0500

    update to employee leave page report

--- Human Relations/Pages/adminLeave.Designer.cs
+++ Human Relations/Pages/adminLeave.Designer.cs
@@ -38,9 +38,10 @@
             // 
             // btnReturn
             // 
-            this.btnReturn.Location = new System.Drawing.Point(9, 16);
+            this.btnReturn.Location = new System.Drawing.Point(14, 25);
+            this.btnReturn.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnReturn.Name = "btnReturn";
-            this.btnReturn.Size = new System.Drawing.Size(75, 25);
+            this.btnReturn.Size = new System.Drawing.Size(112, 38);
             this.btnReturn.TabIndex = 13;
             this.btnReturn.Text = "Return";
             this.btnReturn.UseVisualStyleBackColor = true;
@@ -49,28 +50,31 @@
             // lblDescribe
             // 
             this.lblDescribe.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblDescribe.Location = new System.Drawing.Point(197, 64);
+            this.lblDescribe.Location = new System.Drawing.Point(296, 98);
+            this.lblDescribe.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblDescribe.Name = "lblDescribe";
-            this.lblDescribe.Size = new System.Drawing.Size(400, 20);
+            this.lblDescribe.Size = new System.Drawing.Size(600, 31);
             this.lblDescribe.TabIndex = 11;
-            this.lblDescribe.Text = "View your history";
+            this.lblDescribe.Text = "Manage Leave Requests";
             this.lblDescribe.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // lblTitle
             // 
             this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTitle.Location = new System.Drawing.Point(197, 19);
+            this.lblTitle.Location = new System.Drawing.Point(296, 29);
+            this.lblTitle.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblTitle.Name = "lblTitle";
-            this.lblTitle.Size = new System.Drawing.Size(400, 40);
+            this.lblTitle.Size = new System.Drawing.Size(600, 62);
             this.lblTitle.TabIndex = 12;
             this.lblTitle.Text = "Human Resources";
             this.lblTitle.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // btnLogOut
             // 
-            this.btnLogOut.Location = new System.Drawing.Point(695, 16);
+            this.btnLogOut.Location = new System.Drawing.Point(1042, 25);
+            this.btnLogOut.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnLogOut.Name = "btnLogOut";
-            this.btnLogOut.Size = new System.Drawing.Size(75, 25);
+            this.btnLogOut.Size = new System.Drawing.Size(112, 38);
             this.btnLogOut.TabIndex = 14;
             this.btnLogOut.Text = "Log Out";
             this.btnLogOut.UseVisualStyleBackColor = true;
@@ -78,23 +82,29 @@
             // 
             // adminLeaveDataGrid
             // 
+            this.adminLeaveDataGrid.AllowUserToAddRows = false;
+            this.adminLeaveDataGrid.AllowUserToDeleteRows = false;
             this.adminLeaveDataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.adminLeaveDataGrid.Location = new System.Drawing.Point(29, 204);
+            this.adminLeaveDataGrid.Location = new System.Drawing.Point(44, 314);
+            this.adminLeaveDataGrid.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.adminLeaveDataGrid.MultiSelect = false;
             this.adminLeaveDataGrid.Name = "adminLeaveDataGrid";
-            this.adminLeaveDataGrid.Size = new System.Drawing.Size(726, 345);
+            this.adminLeaveDataGrid.ReadOnly = true;
+            this.adminLeaveDataGrid.RowHeadersWidth = 62;
+            this.adminLeaveDataGrid.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.adminLeaveDataGrid.Size = new System.Drawing.Size(1089, 531);
             this.adminLeaveDataGrid.TabIndex = 16;
             // 
             // adminLeave
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.ClientSize = new System.Drawing.Size(1176, 863);
             this.Controls.Add(this.adminLeaveDataGrid);
             this.Controls.Add(this.btnReturn);
             this.Controls.Add(this.lblDescribe);
             this.Controls.Add(this.lblTitle);
             this.Controls.Add(this.btnLogOut);
-            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "adminLeave";
             this.Text = "Human Resources: Leave Management";
             ((System.ComponentModel.ISupportInitialize)(this.adminLeaveDataGrid)).EndInit();
